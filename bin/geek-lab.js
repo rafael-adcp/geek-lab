d55ac00874ae8651b93d5de193528f5013b638cd
Rafael Prado <rafael_adcp@hotmail.com>
2019-12-20 21:56:50 -0200

    reading actions recursivly from dir dynamically + retreiveing their command action so that we can check for colisions + showing help + error msg when invalid command is provided

--- bin/geek-lab.js
+++ bin/geek-lab.js
@@ -4,25 +4,52 @@ const updateNotifier = require('update-notifier');
 const toString = require('lodash/toString');
 const isEmpty = require('lodash/isEmpty');
 const yargs = require('yargs');
+const recursiveReadSync = require('recursive-readdir-sync');
+const findIndex = require('lodash/findIndex')
 
 const pkg = require(path.join(__dirname, '../package.json'));
 
 updateNotifier({ pkg, updateCheckInterval: 1000 }).notify();
 
+const actionsPath = path.join(__dirname, '../src/actions')
+
+const files = recursiveReadSync(actionsPath);
+  
+const actionNames = [];
+
+// reading default actions from cli
+for(var file of files) {
+    const action = require(file)
+
+    //TODO: handle colision
+  actionNames.push(action.command);
+  /*
+    we cant use 
+    .commandDir(rootPath, { recurse: true }) because we need to know all the commands to prevent 
+    command colisions
+  */
+  yargs
+    .command(action)
+}
+
 const provided = toString(
   Array.prototype.slice.call(process.argv, 2)[0]
 );
 
+const commandExist = findIndex(actionNames, function(o) { return o == provided}) == -1 ? false : true;
+
 //showing cli tool help when an invalid command is provided
-// if (
-//   !isEmpty(provided) &&
-//   provided !== '--help' && //yargs default param to show help
-//   provided !== '--version' //yargs default param to show version
-//   //TODO: !commands[provided] //if command donot exist
-// ) {
-//   console.log(`Invalid command provided "${provided}", see available options below`);
-//   yargs.showHelp();
-// }
+if (
+  !isEmpty(provided) &&
+  provided !== '--help' && //yargs default param to show help
+  provided !== '--version' && //yargs default param to show version
+  !commandExist //if command donot exist
+) {
+  console.log(`Invalid command provided "${provided}", see available options below`);
+  yargs.showHelp();
+}
+
+
 
 yargs
   //apending a message at the botton of help command
@@ -31,6 +58,4 @@ yargs
   .demandCommand(1, '')
   //pretty printing things following to terminal width
   .wrap(yargs.terminalWidth())
-  .commandDir('../src/actions/geek-lab/')
-  .commandDir('../src/actions/')
   .argv;
